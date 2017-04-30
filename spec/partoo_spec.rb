c93ca4a09edb9a0fa6b1fab8fd750e6ef49ec37a
matt.cover <werekraken@gmail.com>
2017-04-30 16:13:34 -0700

    Clean up and add additional rspec tests.

--- spec/partoo_spec.rb
+++ spec/partoo_spec.rb
@@ -5,11 +5,27 @@ RSpec.describe Partoo do
     expect(Partoo::VERSION).not_to be nil
   end
 
-  it "lists files" do
-    expect(Partoo.list("some.par2")).to eql("list of files from some.par2")
+  it "can list files" do
+    expect(Partoo.list('spec/fixtures/files/testdata.par2').to_s).to match(/test-0.data/)
   end
 
-  it "dumps packets" do
+  it "can dump packets" do
     expect(Partoo.dump('spec/fixtures/files/testdata.par2').to_s).to match(/PAR2.*PKT/)
+  end
+
+  it "can get creator" do
+    expect(Partoo.creator('spec/fixtures/files/testdata.par2')).to eql("Created by QuickPar 0.4")
+  end
+
+  it "can get recovery set file count" do
+    expect(Partoo.recovery_set_file_count('spec/fixtures/files/testdata.par2')).to eql(10)
+  end
+
+  it "can get recovery set id" do
+    expect(Partoo.recovery_set_id('spec/fixtures/files/testdata.par2')).to eql(172626095358484736903154840814915262152)
+  end
+
+  it "can get slice size" do
+    expect(Partoo.slice_size('spec/fixtures/files/testdata.par2')).to eql(5376)
   end
 end
