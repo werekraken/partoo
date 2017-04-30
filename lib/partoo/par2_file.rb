b237377734a3def8eef0d5332fabf2e10338c394
matt.cover <werekraken@gmail.com>
2017-04-30 14:56:42 -0700

    Add several new methods to par2_file.rb.

--- lib/partoo/par2_file.rb
+++ lib/partoo/par2_file.rb
@@ -1,4 +1,6 @@
 require 'bindata'
+require 'partoo/crc32'
+require 'zlib'
 
 module Partoo
   class Par2File < BinData::Record
@@ -6,5 +8,56 @@ module Partoo
     require 'partoo/par2_file/packet'
 
     array :packets, type: :packet, read_until: :eof
+
+    def list
+      file_ids.map do |id|
+        f = file_description_packet_by_id(id)[0]['body']
+        crc = ""
+        input_file_slice_checksum_packet_by_id(id)[0]['body']['chunk_checksums'].each do |cc|
+          crc_b = cc['crc32']
+          if crc != ""
+            crc = Zlib.crc32_combine(crc, crc_b, slice_size)
+          else
+            crc = crc_b
+          end
+        end
+        pad_length = slice_size - f.file_length % slice_size
+        crc_z = Zlib.crc32("\0" * pad_length)
+        crc = Partoo::CRC32.crc32_trim_trailing(crc, crc_z, pad_length)
+        [f, {:file_crc32 => crc}]
+      end
+    end
+
+    def creator
+      creator_packet[0]['body']['creator']
+    end
+
+    def file_ids
+      main_packet[0]['body']['file_ids']
+    end
+
+    def slice_size
+      main_packet[0]['body']['slice_size']
+    end
+
+    def recovery_set_file_count
+      main_packet[0]['body']['recovery_set_file_count']
+    end
+
+    def creator_packet
+      packets.select {|p| p['packet_type'] == "PAR 2.0\0Creator\0" }
+    end
+
+    def file_description_packet_by_id(id)
+      packets.select {|p| p['packet_type'] == "PAR 2.0\0FileDesc" && p['body']['file_id'] == id }
+    end
+
+    def input_file_slice_checksum_packet_by_id(id)
+      packets.select {|p| p['packet_type'] == "PAR 2.0\0IFSC\0\0\0\0" && p['body']['file_id'] == id }
+    end
+
+    def main_packet
+      packets.select {|p| p['packet_type'] == "PAR 2.0\0Main\0\0\0\0" }
+    end
   end
 end
