315a807ead1c987584dc9b2d485ef467099a7987
Matt Cover <werekraken@gmail.com>
2017-08-23 02:03:04 -0400

    Improve readability in par2_file.rb.

--- lib/partoo/par2_file.rb
+++ lib/partoo/par2_file.rb
@@ -11,7 +11,7 @@ module Partoo
 
     def list
       file_ids.map do |id|
-        f = file_description_packet_by_id(id)[0]['body']
+        f = file_description_packet_by_id(id)['body']
         { :name   => f.file_name,
           :md5    => f.file_md5,
           :length => f.file_length,
@@ -21,15 +21,15 @@ module Partoo
 
     def to_md5
       file_ids.map do |id|
-        file_description_packet_by_id(id)[0]['body']['file_md5'].to_hex
+        file_description_packet_by_id(id)['body']['file_md5'].to_hex
           .concat('  ')
-          .concat(file_description_packet_by_id(id)[0]['body']['file_name'])
+          .concat(file_description_packet_by_id(id)['body']['file_name'])
       end.join("\n").concat("\n")
     end
 
     def to_sfv
       file_ids.map do |id|
-        file_description_packet_by_id(id)[0]['body']['file_name']
+        file_description_packet_by_id(id)['body']['file_name']
           .concat(' ')
           .concat(crc32_by_id(id).to_s(16).rjust(8, '0'))
       end.join("\n").concat("\n")
@@ -37,7 +37,7 @@ module Partoo
 
     def crc32_by_id(id)
       crc = nil
-      input_file_slice_checksum_packet_by_id(id)[0]['body']['chunk_checksums'].each do |cc|
+      input_file_slice_checksum_packet_by_id(id)['body']['chunk_checksums'].each do |cc|
         crc_b = cc['crc32']
         if crc.nil?
           crc = crc_b
@@ -45,47 +45,55 @@ module Partoo
           crc = Zlib.crc32_combine(crc, crc_b, slice_size)
         end
       end
-      pad_length = slice_size - file_description_packet_by_id(id)[0]['body'].file_length % slice_size
+      pad_length = slice_size - file_description_packet_by_id(id)['body'].file_length % slice_size
       crc_z = Zlib.crc32("\0" * pad_length)
       Partoo::CRC32.crc32_trim_trailing(crc, crc_z, pad_length)
     end
 
     def creator
-      creator_packet[0]['body']['creator']
+      creator_packet['body']['creator']
     end
 
     def file_ids
-      main_packet[0]['body']['file_ids'].sort_by do |id|
-        file_description_packet_by_id(id)[0]['body']['file_name']
+      main_packet['body']['file_ids'].sort_by do |id|
+        file_description_packet_by_id(id)['body']['file_name']
       end
     end
 
     def slice_size
-      main_packet[0]['body']['slice_size']
+      main_packet['body']['slice_size']
     end
 
     def recovery_set_file_count
-      main_packet[0]['body']['recovery_set_file_count']
+      main_packet['body']['recovery_set_file_count']
     end
 
     def recovery_set_id
-      main_packet[0]['recovery_set_id']
+      main_packet['recovery_set_id']
     end
 
     def creator_packet
-      packets.select {|p| p['packet_type'] == "PAR 2.0\0Creator\0" }
+      packets.select do |p|
+        p['packet_type'] == "PAR 2.0\0Creator\0"
+      end.first
     end
 
     def file_description_packet_by_id(id)
-      packets.select {|p| p['packet_type'] == "PAR 2.0\0FileDesc" && p['body']['file_id'] == id }
+      packets.select do |p|
+        p['packet_type'] == "PAR 2.0\0FileDesc" && p['body']['file_id'] == id
+      end.first
     end
 
     def input_file_slice_checksum_packet_by_id(id)
-      packets.select {|p| p['packet_type'] == "PAR 2.0\0IFSC\0\0\0\0" && p['body']['file_id'] == id }
+      packets.select do |p|
+        p['packet_type'] == "PAR 2.0\0IFSC\0\0\0\0" && p['body']['file_id'] == id
+      end.first
     end
 
     def main_packet
-      packets.select {|p| p['packet_type'] == "PAR 2.0\0Main\0\0\0\0" }
+      packets.select do |p|
+        p['packet_type'] == "PAR 2.0\0Main\0\0\0\0"
+      end.first
     end
   end
 end
