a91eeacbd1175f34c0f20058c8891d5f38c16d7d
Matt Cover <werekraken@gmail.com>
2017-09-16 04:02:40 -0400

    Show progress and pad result only if stdout is a tty.

--- lib/partoo/cli.rb
+++ lib/partoo/cli.rb
@@ -61,10 +61,12 @@ module Partoo
 
     desc "verify <par2 file>", "Verify source files found in a par2 file"
     def verify(par2_file)
+      pad = ""
+      pad << "   " if $stdout.tty?
       Partoo.verify(par2_file) do |source_file, bytes_read, result|
         percent_complete = '%.2f' % [ bytes_read.to_f / source_file.real_size.to_f * 100 ]
-        print "#{source_file.name} #{percent_complete}%\r"
-        puts "#{source_file.name} #{result}   " if !result.nil?
+        print "#{source_file.name} #{percent_complete}%\r" if $stdout.tty?
+        puts "#{source_file.name} #{result}#{pad}" if !result.nil?
       end
     end
   end
