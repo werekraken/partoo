42c7a9b9ae7191fe14d11b49cd36c1e5c5e53a5e
Matt Cover <werekraken@gmail.com>
2017-09-16 13:05:34 -0400

    Add tests for Partoo::Par2File{#read,.open}.

---
+++ spec/par2_file_spec.rb
@@ -0,0 +1,18 @@
+require "partoo/par2_file"
+
+RSpec.describe Partoo::Par2File do
+
+  describe '#read' do
+    it 'returns an instance of Partoo::Par2File' do
+      io = File.open('spec/fixtures/files/testdata.par2')
+
+      expect(Partoo::Par2File.read(io)).to be_an_instance_of(Partoo::Par2File)
+    end
+  end
+
+  describe '.open' do
+    it 'returns an instance of Partoo::Par2File' do
+      expect(Partoo::Par2File.open('spec/fixtures/files/testdata.par2')).to be_an_instance_of(Partoo::Par2File)
+    end
+  end
+end
