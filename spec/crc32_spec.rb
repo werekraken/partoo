db8d29793bfa9213eb8b398cc9346cb0f1917947
Matt Cover <werekraken@gmail.com>
2017-09-16 03:44:36 -0400

    Add tests for Partoo::CRC32#{finish,reset,update}.

--- spec/crc32_spec.rb
+++ spec/crc32_spec.rb
@@ -1,6 +1,51 @@
 require "partoo/crc32"
 
 RSpec.describe Partoo::CRC32 do
+
+  describe '#finish' do
+    it 'packs the crc' do
+      contents = "this\n"
+      crc = Partoo::CRC32.new
+      crc.update(contents)
+      zcrc = Zlib.crc32(contents).to_s(16).rjust(8, '0')
+
+      expect(Digest.hexencode(crc.finish)).to eq zcrc
+    end
+  end
+
+  describe '#reset' do
+    it 'reinitializes the crc' do
+      fresh_crc = Partoo::CRC32.new
+      reset_crc = Partoo::CRC32.new
+      reset_crc.update("this\n")
+      reset_crc.reset
+
+      expect(reset_crc).to eq fresh_crc
+    end
+  end
+
+  describe '#update' do
+    it 'calculates crc of supplied data' do
+      contents = "this\n"
+      crc = Partoo::CRC32.new
+      crc.update(contents)
+      zcrc = Zlib.crc32(contents).to_s(16).rjust(8, '0')
+
+      expect(crc.hexdigest).to eq zcrc
+    end
+
+    it 'adds to previous checksum' do
+      contentsA = "this\n"
+      contentsB = "that\n"
+      crc = Partoo::CRC32.new
+      crc.update(contentsA)
+      crc.update(contentsB)
+      zcrc = Zlib.crc32(contentsA.concat(contentsB)).to_s(16).rjust(8, '0')
+
+      expect(crc.hexdigest).to eq zcrc
+    end
+  end
+
   describe '.crc32_trim_trailing' do
     it 'returns crcA when given crcAB, crcB, and lengthB.' do
       contentsA = "this\n"
