35cab178c584ef9b2be498423122fc69a017f4dd
Matt Cover <werekraken@gmail.com>
2017-09-17 00:03:10 -0400

    Add test for Partoo::Par2File#creator.

--- spec/par2_file_spec.rb
+++ spec/par2_file_spec.rb
@@ -2,6 +2,14 @@ require "partoo/par2_file"
 
 RSpec.describe Partoo::Par2File do
 
+  describe '#creator' do
+    it 'returns the creator' do
+      f = Partoo::Par2File.open('spec/fixtures/files/testdata.par2')
+
+      expect(f.creator).to eq 'Created by QuickPar 0.4'
+    end
+  end
+
   describe '#read' do
     it 'returns an instance of Partoo::Par2File' do
       io = File.open('spec/fixtures/files/testdata.par2')
